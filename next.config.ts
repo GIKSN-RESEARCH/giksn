960c9f92fcc9339bb9d37189317c502db153e2db
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-06-17 21:17:43 +0530

    configs & dependencies

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: process.cwd(),
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/favicon.png",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
